4fd5e64271a455e372334921bd0d29d1fe832588
Leon Seipp <l.seipp@microeinhundert.com>
2019-05-06 08:08:17 +0200

    Removed API key

--- src/api/providers/youtube/index.js
+++ src/api/providers/youtube/index.js
@@ -6,7 +6,8 @@ const youtube = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
     part: 'snippet',
-    key: 'AIzaSyDV-D04s-RbQDwPXxadnt2LKpuC1rPveUc',
+    /* Insert API key here */
+    key: '',
   },
 });
 
